ea4e46369d6f387e56e8879d4c10808287a8eba1
Tyler Henry <tyler.henry2@dcmail.ca>
2021-11-26 20:05:01 -0500

    working RandomVector, test in main

--- src/ca/durhamcollege/RandomVector.java
+++ src/ca/durhamcollege/RandomVector.java
@@ -1,6 +1,23 @@
 package ca.durhamcollege;
+import java.util.Random;
 
 public class RandomVector
 {
 
+    public static Vector2D createVector2D(Vector2D start, Vector2D end)
+    {
+        Random rand = new Random(); // instance of random class
+
+        //generate random x value
+        float minX = Math.min(start.getX(), end.getX());
+        float maxX = Math.max(start.getX(), end.getX());
+        float randomX = (rand.nextFloat() * maxX) + minX;
+
+        //generate random y value
+        float minY = Math.min(start.getY(), end.getY());
+        float maxY = Math.max(start.getY(), end.getY());
+        float randomY = (rand.nextFloat() * maxY) + minY;
+
+        return new Vector2D(randomX, randomY);
+    }
 }
